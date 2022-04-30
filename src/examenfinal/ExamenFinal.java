5065d68608e737e4a91cb47525d43ed09bc1dfc6
SmillerMP <samuelmp7@gmail.com>
2022-04-29 22:54:40 -0600

    Respuestas
    se añadio el pdf con las capturas de pantalla del funcionamiento del programa

--- src/examenfinal/ExamenFinal.java
+++ src/examenfinal/ExamenFinal.java
@@ -20,80 +20,76 @@ public class ExamenFinal {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
-        
+
         do {
-        // Espaciado    
-        System.out.print("\n\n\n");
-            
-        //Menu de Inicio
-        System.out.println("=========== Examen Final ===========");
-        System.out.println("1. Problema 1");
-        System.out.println("2. Problema 2");
-        System.out.println("3. Problema 3");
-        System.out.println("4. Salir");
+            // Espaciado    
+            System.out.print("\n\n\n");
 
-        // Seleccion de opciones
-        System.out.print("Ingrese una opcion: ");
-        opcion = sc.nextInt();
-        
-        System.out.print("\n");
-        
-        
-        switch(opcion){
-            
-            // Primer Programa
-            case 1:
-                // Ingreso del primer numero
-                System.out.print("Ingrese un Primer numero: ");
-                int n1 = sc.nextInt();
-                
-                // Ingreso del segundo numero
-                System.out.print("Ingrese un Segundo numero: ");
-                int n2 = sc.nextInt();
-                
-                System.out.print("\n");
-                
-                // Comparacion de numero mayor
-                if(n1 > n2){
-                    System.out.print("El numero Mayor es: " + n1);
-                } else {
-                    System.out.print("El numero Mayor es: " + n2);
-                }
-                
-                System.out.print("\n\n\n");
-                break;
-           
-                
-            case 2:
-                
-                System.out.print("Ingrese el tama;o de la Piramide: ");
-                int piramide = sc.nextInt();
+            //Menu de Inicio
+            System.out.println("=========== Examen Final ===========");
+            System.out.println("1. Problema 1");
+            System.out.println("2. Problema 2");
+            System.out.println("3. Problema 3");
+            System.out.println("4. Salir");
 
-                if (piramide % 2 != 0) {
-                    int asterisco = 1;
-                    int espaciador = (piramide / 2) + 1;
-                    for (int i = 0; i < (piramide / 2) + 1; i++) {
-                        espaciador--;
-                        for (int j = 0; j < espaciador; j++) {
-                            System.out.print(" ");
-                        }
-                        for (int k = 0; k < asterisco; k++) {
-                            System.out.print("*");
-                        }
-                        asterisco = asterisco + 2;
-                        System.out.print("\n");
+            // Seleccion de opciones
+            System.out.print("Ingrese una opcion: ");
+            opcion = sc.nextInt();
+
+            System.out.print("\n");
+
+            switch (opcion) {
+
+                // Primer Programa
+                case 1:
+                    // Ingreso del primer numero
+                    System.out.print("Ingrese un Primer numero: ");
+                    int n1 = sc.nextInt();
+
+                    // Ingreso del segundo numero
+                    System.out.print("Ingrese un Segundo numero: ");
+                    int n2 = sc.nextInt();
+
+                    System.out.print("\n");
+
+                    // Comparacion de numero mayor
+                    if (n1 > n2) {
+                        System.out.print("El numero Mayor es: " + n1);
+                    } else {
+                        System.out.print("El numero Mayor es: " + n2);
                     }
-                } else {
-                    System.out.print("Solo se aceptan numeros impares");
-                }
-                
-                
-                
-                
-                
-        }
-            
-        }while(opcion < 4);
+
+                    System.out.print("\n\n\n");
+                    break;
+
+                case 2:
+
+                    System.out.print("Ingrese el tama;o de la Piramide: ");
+                    int piramide = sc.nextInt();
+
+                    if (piramide % 2 != 0) {
+                        int asterisco = 1;
+                        int espaciador = (piramide / 2) + 1;
+                        for (int i = 0; i < (piramide / 2) + 1; i++) {
+                            espaciador--;
+                            for (int j = 0; j < espaciador; j++) {
+                                System.out.print(" ");
+                            }
+                            for (int k = 0; k < asterisco; k++) {
+                                System.out.print("*");
+                            }
+                            asterisco = asterisco + 2;
+                            System.out.print("\n");
+                        }
+                    } else {
+                        System.out.print("Solo se aceptan numeros impares");
+                    }
+                    
+                    break;
+                    
+                case 3:
+                    
+            }
+        } while (opcion < 4);
     }
-    
 }
