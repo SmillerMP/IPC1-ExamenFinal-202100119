5cf8186226d0bdfd3b58a75834ce8ade52109775
SmillerMP <samuelmp7@gmail.com>
2022-04-29 21:45:43 -0600

    Problema 1 y 2
    resolucion al problema 1 y 2

--- src/examenfinal/ExamenFinal.java
+++ src/examenfinal/ExamenFinal.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 
 /**
  *
- * @author Samuellllll
+ * @author Samuel MP
+ * @link 
+ * @Tema Examen Final
  */
 public class ExamenFinal {
 
@@ -30,8 +32,66 @@ public class ExamenFinal {
         System.out.println("3. Problema 3");
         System.out.println("4. Salir");
 
+        // Seleccion de opciones
         System.out.print("Ingrese una opcion: ");
         opcion = sc.nextInt();
+        
+        System.out.print("\n");
+        
+        
+        switch(opcion){
+            
+            // Primer Programa
+            case 1:
+                // Ingreso del primer numero
+                System.out.print("Ingrese un Primer numero: ");
+                int n1 = sc.nextInt();
+                
+                // Ingreso del segundo numero
+                System.out.print("Ingrese un Segundo numero: ");
+                int n2 = sc.nextInt();
+                
+                System.out.print("\n");
+                
+                // Comparacion de numero mayor
+                if(n1 > n2){
+                    System.out.print("El numero Mayor es: " + n1);
+                } else {
+                    System.out.print("El numero Mayor es: " + n2);
+                }
+                
+                System.out.print("\n\n\n");
+                break;
+           
+                
+            case 2:
+                
+                System.out.print("Ingrese el tama;o de la Piramide: ");
+                int piramide = sc.nextInt();
+
+                if (piramide % 2 != 0) {
+                    int asterisco = 1;
+                    int espaciador = (piramide / 2) + 1;
+                    for (int i = 0; i < (piramide / 2) + 1; i++) {
+                        espaciador--;
+                        for (int j = 0; j < espaciador; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 0; k < asterisco; k++) {
+                            System.out.print("*");
+                        }
+                        asterisco = asterisco + 2;
+                        System.out.print("\n");
+                    }
+                } else {
+                    System.out.print("Solo se aceptan numeros impares");
+                }
+                
+                
+                
+                
+                
+        }
             
         }while(opcion < 4);
     }
